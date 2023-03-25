b64d1f3f4f77ea701baf940c8f00fd053b79069b
Scott Stephens <stephens.js@gmail.com>
2023-03-25 11:30:51 -0500

    Add basic components from gatsby-starter-lanyon

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,9 @@
-import React from "react"
+import React from 'react';
 
 import Layout from '../components/layout';
 
 export default () => (
     <Layout>
-        <h1>Index Page</h1>
+        <h1>About</h1>
     </Layout>
 )
